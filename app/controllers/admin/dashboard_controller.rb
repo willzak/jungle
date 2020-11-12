fc63ca66b0c34a8c73122219416fbdd7b5c31b6c
William <williamzak0@gmail.com>
2020-11-12 01:19:45 +0000

    added counts to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @count_products = Product.count
+    @count_categories = Category.count
   end
+
 end
