5e26912c76a0b6655d0ab9c14524f1f3a6005974
William <williamzak0@gmail.com>
2020-11-12 19:30:25 +0000

    added sign up and login functions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      @error = false
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      @error = true
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+end
